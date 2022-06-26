2e8a2f3de6e88c73db861fb12e8e4a2326bc1e6e
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-06-25 18:22:29 -0700

    refactor gl operations

---
+++ components/sat-vis.js
@@ -0,0 +1,116 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { createCanvas } from 'canvas'
+import { mat4 } from 'gl-matrix'
+import useWindowDim from '../hooks/window-dim.js'
+import { loadShader, createProgram, switchShader, initAttribute, initBuffer } from '../lib/glu.js'
+
+import styles from '../styles/SatVis.module.css'
+
+
+const SatVis = props => {
+    const { width, height } = useWindowDim()
+    const canvRef = useRef()
+
+    const glRef = useRef()
+    const programRef = useRef({})
+    const locationRef = useRef({
+        buffer: {},
+        attrib: {}
+    })
+
+    const modelMatRef = useRef(mat4.create())
+    const viewMatrix = mat4.lookAt(mat4.create(), 
+        [0, 0, 10], // camera position
+        [0, 0, 0], // camera focus
+        [0, 1, 0] // up vector
+    )
+    const getProjMat = (aspect) => {
+        return mat4.perspective(mat4.create(),
+            90, //fov
+            aspect,
+            .1, //near
+            100 //far
+        )
+    }
+    
+    const byteSize = Float32Array.BYTES_PER_ELEMENT
+
+    const frameIdRef = useRef()
+    const requestFrame = func => { frameIdRef.current = window.requestAnimationFrame(func) }
+    const cancelFrame = () => { if(frameIdRef.current) window.cancelAnimationFrame(frameIdRef.current) }
+
+    const initPrograms = async (gl) => {
+        const pointVert = await loadShader(gl, gl.VERTEX_SHADER, './shaders/pointVert.glsl')
+        const pointFrag = await loadShader(gl, gl.FRAGMENT_SHADER, './shaders/pointFrag.glsl')
+        programRef.current['point'] = createProgram(gl, pointVert, pointFrag)
+    }
+
+    const initShaderVars = (gl, programs) => {
+        switchShader(gl, programs.point)
+        locationRef.current.attrib['point'] = {}
+        locationRef.current.attrib.point['aPosition'] = initAttribute(gl, 'aPosition', 3, 3, 0, false, byteSize)
+
+        gl.uniformMatrix4fv(gl.getUniformLocation(gl.program, 'uModelMatrix'), false, modelMatRef.current)
+        gl.uniformMatrix4fv(gl.getUniformLocation(gl.program, 'uViewMatrix'), false, viewMatrix)
+    }
+
+    const initBuffers = (gl) => {
+        locationRef.current.buffer['point'] = initBuffer(gl, props.data, gl.STATIC_DRAW)
+    }
+
+    const setupViewport = (gl, programs) => {
+        const {innerWidth: w, innerHeight: h, devicePixelRatio: dpr } = window
+        gl.viewport(0, 0, w * dpr, h * dpr)
+        const projMatrix = getProjMat((w * dpr)/(h * dpr))
+    
+        if(programs?.point) {
+            switchShader(gl, programs.point)
+            gl.uniformMatrix4fv(gl.getUniformLocation(gl.program, 'uProjMatrix'), false, projMatrix)
+        }
+    }
+
+    const initGl = async () => {
+        if (glRef.current) return
+
+        glRef.current = canvRef.current.getContext('webgl', { preserveDrawingBuffer: false })
+
+        await initPrograms(glRef.current)
+        initShaderVars(glRef.current, programRef.current)
+        initBuffers(glRef.current)
+        setupViewport(glRef.current, programRef.current)
+
+        requestFrame(draw)
+    }
+
+    const draw = time => {
+        const gl = glRef.current
+        const { buffer, attrib } = locationRef.current
+
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT)
+
+        if (buffer?.point) {
+            switchShader(gl, programRef.current.point)
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer.point)
+            gl.vertexAttribPointer(attrib.point.aPosition, 3, gl.FLOAT, false, 3 * byteSize, 0)
+            gl.drawArrays(gl.POINTS, 0, props.data.length / 3)
+        }
+
+        requestFrame(draw)
+    }
+
+    useEffect(() => {
+        initGl()
+        return cancelFrame
+    }, [])
+
+    useEffect(() => {
+        setupViewport(glRef.current, programRef.current)
+    }, [width, height])
+
+
+    return (
+        <canvas className={styles.vis} ref={canvRef} width={width} height={height}></canvas>
+    )
+}
+
+export default SatVis
