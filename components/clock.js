e19857dff7902f6b0cb2ac8d347bbb77a87e638f
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-07-09 11:28:56 -0600

    add clock component

---
+++ components/clock.js
@@ -0,0 +1,43 @@
+import React, { useRef, useEffect } from 'react'
+
+const Clock = props => {
+    const frameIdRef = useRef()
+    const epochRef = useRef()
+
+    const clockSpeed = 100
+    const tickRate = 1000/60
+    const msPerDay = 86400000
+
+    let lastT = 0
+    const update = time => {
+        const elapsed = time - lastT
+        if (elapsed < tickRate) {
+            frameIdRef.current = window.requestAnimationFrame(update)
+            return
+        }
+        lastT = time
+
+        epochRef.current += elapsed/msPerDay * clockSpeed
+        props.setTime(epochRef.current)
+
+        frameIdRef.current = window.requestAnimationFrame(update)
+    }
+
+    useEffect(() => {
+        const currT = new Date()
+        const startT = new Date(Date.UTC(currT.getFullYear(), 0))
+        epochRef.current = (currT - startT)/msPerDay
+        props.setTime(epochRef.current)
+
+        frameIdRef.current = window.requestAnimationFrame(update)
+        return () => {
+            window.cancelAnimationFrame(frameIdRef.current)
+        }
+    }, [])
+
+    return (
+        <></>
+    )
+}
+
+export default Clock
