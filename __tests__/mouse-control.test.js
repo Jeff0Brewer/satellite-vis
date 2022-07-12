321996fc20d1994e173bc4a343b971071233e885
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-07-12 13:28:42 -0600

    add mouse control tests

---
+++ __tests__/mouse-control.test.js
@@ -0,0 +1,55 @@
+import { mouseRotate, scrollZoom } from '../lib/mouse-control.js'
+import { mat4 } from 'gl-matrix'
+
+test('mouseRotate returns same matrix on 360 rotation', () => {
+    const startMatrix = mat4.create()
+    const endMatrix = mouseRotate(startMatrix, Math.PI*2, Math.PI*2, 1)
+    endMatrix.forEach((n, i) => {
+        expect(startMatrix[i]).toBeCloseTo(n)
+    })
+})
+
+test('mouseRotate rotates about Z on X movement', () => {
+    const mat = mouseRotate(mat4.create(), Math.PI, 0, 1)
+    const comp = mat4.fromZRotation(mat4.create(), Math.PI)
+    comp.forEach((n, i) => {
+        expect(mat[i]).toBeCloseTo(n)
+    })
+})
+
+test('mouseRotate rotates about X on Y movement', () => {
+    const mat = mouseRotate(mat4.create(), 0, Math.PI, 1)
+    const comp = mat4.fromXRotation(mat4.create(), -Math.PI)
+    comp.forEach((n, i) => {
+        expect(mat[i]).toBeCloseTo(n)
+    })
+})
+
+test('mouseRotate magnitude impacted by speed parameter', () => {
+    const startMatrix = mat4.create()
+    const endMatrix = mouseRotate(startMatrix, Math.PI*4, Math.PI*4, 0.5)
+    endMatrix.forEach((n, i) => {
+        expect(startMatrix[i]).toBeCloseTo(n)
+    })
+})
+
+test('scrollZoom returns same matrix on 0 scroll', () => {
+    const startMatrix = mat4.create()
+    const endMatrix = scrollZoom(startMatrix, 0, 1)
+    endMatrix.forEach((n, i) => {
+        expect(startMatrix[i]).toBeCloseTo(n)
+    })
+})
+
+test('scrollZoom scales on nonzero scroll', () => {
+    const scroll = 5
+    const mat = scrollZoom(mat4.create(), scroll, 1)
+    expect(mat[0]).toBe(scroll + 1)
+})
+
+test('scrollZoom magnitude impacted by speed parameter', () => {
+    const speed = 2
+    const scroll = 5
+    const mat = scrollZoom(mat4.create(), scroll, speed)
+    expect(mat[0]).toBe(scroll*speed + 1)
+})
