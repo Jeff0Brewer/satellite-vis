24edfafdefb9d9f484f65b2adf6f562922cfbb0e
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-07-28 17:27:47 -0600

    add test for anomaly + perigee

--- __tests__/keplerian.test.js
+++ __tests__/keplerian.test.js
@@ -25,8 +25,20 @@ const tles = [
         "1 31738U 99025CEM 22206.38499252  .00001197  00000+0  83829-3 0  9991",
         "2 31738  99.4627 261.3448 0333568 129.3385 233.7835 13.76658651452008"
     ],[
-        "1 50213U 21126B   22008.72139705  .00001997  67395-6  41610-3 0  9996",
-        "2 50213  26.9105 333.9384 8403639 185.2217 148.3798  1.04295033   195"
+        "1 48928U 21059BB  22206.36763380  .00004325  00000+0  22434-3 0  9997",
+        "2 48928  97.5616 336.5942 0013856 332.2799  27.7694 15.16607442 59721"
+    ],[
+        "1 35932U 09051B   22206.56210687  .00000411  00000+0  10262-3 0  9993",
+        "2 35932  98.5651  63.5568 0006414 296.0587  63.9949 14.56907919681649"
+    ],[
+        "1 38340U 12025D   22206.15464114  .00001026  00000+0  15405-3 0  9992",
+        "2 38340  98.1940 285.1122 0012722 106.0841 254.1769 14.76730318537803"
+    ],[
+        "1 48272U 21034E   22206.53527987  .00002106  00000+0  23843-3 0  9992",
+        "2 48272  97.7456 277.1571 0002412  80.3836 279.7632 14.87463315 67225"
+    ],[
+        "1 43669U 18083H   22206.52050676  .00008739  00000+0  38248-3 0  9995",
+        "2 43669  97.4132 230.6424 0009360 186.5161 330.0759 15.22244358207094"
     ]
 ]
 
@@ -95,26 +107,19 @@ describe('elementsFromPosVel returns correct semimajor axis at t=0', () => {
     )
 })
 
-//describe('elementsFromPosVel returns correct anomaly axis at t=0', () => {
-//    test.each(tles)(
-//        'tle: \n %p \n %p',
-//        (line1, line2) => {
-//            const satrec = twoline2satrec(line1, line2)
-//            const { position, velocity } = sgp4(satrec, 0)
-//            const elements = elementsFromPosVel(position, velocity)
-//            expect(elements.anom).toBeCloseTo(getMeanAnomaly(line1, line2), 2)
-//        }
-//    )
-//})
+describe('elementsFromPosVel returns correct overall angle at t=0', () => {
+    test.each(tles)(
+        'tle: \n %p \n %p',
+        (line1, line2) => {
+            const satrec = twoline2satrec(line1, line2)
+            const { position, velocity } = sgp4(satrec, 0)
+            const elements = elementsFromPosVel(position, velocity)
 
-//describe('elementsFromPosVel returns correct argument of perigee at t=0', () => {
-//    test.each(tles)(
-//        'tle: \n %p \n %p',
-//        (line1, line2) => {
-//            const satrec = twoline2satrec(line1, line2)
-//            const { position, velocity } = sgp4(satrec, 0)
-//            const elements = elementsFromPosVel(position, velocity)
-//            expect(elements.argp).toBeCloseTo(getArgumentPerigee(line1, line2), 2)
-//        }
-//    )
-//})
+            const calcAngle = (elements.anom + elements.argp) % (Math.PI*2)
+            const tleAngle = (getArgumentPerigee(line1, line2) + getMeanAnomaly(line1, line2)) % (Math.PI*2)
+            const offsetPi = angle => (angle + Math.PI) % (2*Math.PI)
+            const angleDiff = Math.min(Math.abs(tleAngle - calcAngle), Math.abs(offsetPi(tleAngle) - offsetPi(calcAngle)))
+            expect(angleDiff).toBeLessThan(Math.PI/10)
+        }
+    )
+})
