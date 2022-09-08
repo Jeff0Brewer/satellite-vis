ffa34b66056017d866816308b2f979b162ee1631
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-09-07 18:14:59 -0600

    move clock and state to home component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
-import styles from '../styles/Home.module.css'
+import { useState, useRef } from 'react'
 import Visualization from '../components/visualization.js'
+import Clock from '../components/clock.js'
 import Catalog from '../components/catalog.js'
+import { newEpoch } from '../lib/shared-epoch.js'
+import styles from '../styles/Home.module.css'
 
 const Home = () => {
-    const [satData, setSatData] = useState(new Float32Array())
+    const [satData, setSatData] = useState([])
+    const [clockSpeed, setClockSpeed] = useState(0)
+    const sharedEpochRef = useRef(newEpoch(new Date()))
 
     return (
         <main className={styles.home}>
-            <Visualization data={satData} />
+            <Clock sharedEpoch={sharedEpochRef.current} setSpeed={setClockSpeed} />
             <Catalog setData={setSatData} />
+            <Visualization data={satData} clockSpeed={clockSpeed} epoch={sharedEpochRef.current} />
         </main>
     )
 }
