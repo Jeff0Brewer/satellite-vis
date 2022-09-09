25e7826f6667ca32202e6fb453552c1467373e8e
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-09-09 11:57:43 -0600

    move catalog entries to component, add page controls

--- components/catalog.js
+++ components/catalog.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { GiOrbit } from 'react-icons/gi'
-import { FaRegEye, FaBan } from 'react-icons/fa'
+import { FaRegEye, FaBan, FaCaretLeft, FaCaretRight } from 'react-icons/fa'
 import { satCategories } from '../util/celes-groups.js'
 import MultiSelect from './multi-select.js'
 import styles from '../styles/Catalog.module.css'
@@ -8,6 +8,7 @@ import styles from '../styles/Catalog.module.css'
 const Catalog = props => {
     const [dataset, setDataset] = useState([])
     const [currPage, setCurrPage] = useState(0)
+    const [maxPage, setMaxPage] = useState(0)
     const ITEM_PER_PAGE = 20
 
     const [nameSearch, setNameSearch] = useState('')
@@ -23,18 +24,24 @@ const Catalog = props => {
         setSelectedCategories(selected)
     }
 
+    const setVisData = data => {
+        props.setData(data)
+        setMaxPage(Math.floor(data.length/ITEM_PER_PAGE))
+        setCurrPage(0)
+    }
+
     useEffect(() => {
         fetch('/api/get-tles')
             .then(res => res.json())
             .then(data => {
                 setDataset(data)
-                props.setData(data)
+                setVisData(data)
             })
             .catch(err => console.log(err))
     }, [])
 
     useEffect(() => {
-        props.setData(dataset.filter(item => {
+        setVisData(dataset.filter(item => {
             return selectedCategories.has(item.category) &&
                 item.name.toLowerCase().includes(nameSearch) &&
                 item.satelliteId.toString().includes(idSearch)
@@ -83,21 +90,38 @@ const Catalog = props => {
             </span>
             <section className={styles.list}>{
                 props.data.slice(currPage*ITEM_PER_PAGE, (currPage+1)*ITEM_PER_PAGE)
-                    .map((item, i) => (
-                        <span key={i} className={styles.listItem}>
-                            <p className={styles.labelLarge}>{item.name}</p>
-                            <p className={styles.labelLarge}>{item.satelliteId}</p>
-                            <p className={styles.labelLarge}>{item.category}</p>
-                            <button className={`${styles.labelSmall} ${styles.unselected}`}>
-                                <GiOrbit />
-                            </button>
-                            <button className={`${styles.labelSmall} ${styles.unselected}`}>
-                                <FaRegEye />
-                            </button>
-                        </span>
-                    ))
+                    .map((item, i) => <CatalogItem item={item} key={i} />) }{
+                maxPage > 0 ?
+                <span>
+                    <div>
+                        <button onClick={() => setCurrPage(Math.max(currPage - 1, 0))}>
+                            <FaCaretLeft />
+                        </button>
+                        <p>{`${currPage + 1}/${maxPage + 1}`}</p>
+                        <button onClick={() => setCurrPage(Math.min(currPage + 1, maxPage))}>
+                            <FaCaretRight />
+                        </button>
+                    </div>
+                </span> :
+                <></>
             }</section>
         </section>
+    )
+}
+
+const CatalogItem = props => {
+    return (
+        <span className={styles.listItem}>
+            <p className={styles.labelLarge}>{props.item.name}</p>
+            <p className={styles.labelLarge}>{props.item.satelliteId}</p>
+            <p className={styles.labelLarge}>{props.item.category}</p>
+            <button className={`${styles.labelSmall} ${styles.unselected}`}>
+                <GiOrbit />
+            </button>
+            <button className={`${styles.labelSmall} ${styles.unselected}`}>
+                <FaRegEye />
+            </button>
+        </span>
     )
 }
 
