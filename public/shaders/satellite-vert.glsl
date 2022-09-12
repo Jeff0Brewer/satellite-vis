2ef3ae09e7f71388a6df7281f2cfc6471e5ad1e2
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-09-12 17:41:42 -0600

    color satellites by category

--- public/shaders/satellite-vert.glsl
+++ public/shaders/satellite-vert.glsl
@@ -1,10 +1,14 @@
 attribute vec4 aPosition;
+attribute vec4 aColor;
 
 uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
 uniform mat4 uProjMatrix;
 
+varying vec4 vColor;
+
 void main() {
     gl_Position = uProjMatrix * uViewMatrix * uModelMatrix * aPosition;
-    gl_PointSize = 10.0/pow(gl_Position.w, 0.7);
+    gl_PointSize = 10.0/pow(gl_Position.w, 0.4);
+    vColor = aColor;
 }
