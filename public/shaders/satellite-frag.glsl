aa1dfa0c41cdbd746f62103c4fe08b02a6c983f9
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-12-01 13:18:13 -0700

    tune point discard radius

--- public/shaders/satellite-frag.glsl
+++ public/shaders/satellite-frag.glsl
@@ -8,7 +8,7 @@ void main() {
     float radius = dot(center, center);
     float delta = fwidth(radius);
     float alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, radius);
-    if (radius > 1.0 + .5*delta) {
+    if (radius > 1.0 + .2 * delta) {
         discard;
     }
     gl_FragColor = vec4(vColor.xyz, alpha);
