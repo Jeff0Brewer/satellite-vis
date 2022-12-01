e200c90fc75147f50f6a8ed2d6d5f0ce8a9de692
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-12-01 15:14:27 -0700

    rename variable

--- public/shaders/satellite-frag.glsl
+++ public/shaders/satellite-frag.glsl
@@ -4,8 +4,8 @@ precision highp float;
 varying vec4 vColor;
 
 void main() {
-    vec2 center = 2.0 * gl_PointCoord - 1.0;
-    float radius = dot(center, center);
+    vec2 coord = 2.0 * gl_PointCoord - 1.0;
+    float radius = dot(coord, coord);
     float delta = fwidth(radius);
     float alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, radius);
     if (radius > 1.0 + .2 * delta) {
