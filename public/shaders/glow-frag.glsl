8ed084ffb2920053885f5121d0372a080c00f387
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-12-01 16:35:37 -0700

    increase glow intensity

--- public/shaders/glow-frag.glsl
+++ public/shaders/glow-frag.glsl
@@ -3,7 +3,7 @@ precision highp float;
 void main() {
     vec2 coord = 2.0 * gl_PointCoord - 1.0;
     float invRadius = 1.0 - dot(coord, coord);
-    float glow = .75 * sign(invRadius)*abs(pow(invRadius, 2.5));
+    float glow = sign(invRadius)*abs(pow(invRadius, 2.5));
 
     gl_FragColor = vec4(.4, .7, 1.0, glow);
 }
