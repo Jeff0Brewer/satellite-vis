2f744b53cab2412929dff06522ace2196c7d0810
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-12-02 22:09:16 -0700

    Revert "prevent mobile zooming"
    This reverts commit f48a0c5a1348b28bee3b68da021e03abccb1037e.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,8 @@
 import React from 'react'
-import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <title>Satellite Visualization</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-                <link rel="icon" type="image/png" href="./favicon.png"/>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    )
+    return <Component {...pageProps} />
 }
 
 export default MyApp
